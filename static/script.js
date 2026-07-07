1bada54c441171867de13e909af7aa5013ade5fb
moluskman <thenuka.se@gmail.com>
2026-07-07 12:20:55 +1000

    fixed search function

--- static/script.js
+++ static/script.js
@@ -1,8 +1,16 @@
 async function searchDatabase() {
     const query = document.getElementById('search-form').querySelector('input[type="search"]').value;
-    const response = await fetch(`/api/search?q=${query}`);
-    const results = await response.json();
-    displayResults(results);
 
-    console.log(data.results);
+    const response = await fetch(`/api/search?q=${query}`);
+    const dataObj = await response.json();
+
+    displayResults(dataObj);
+
+    console.log(dataObj.results);
 }
+
+const mediaForm = document.getElementById('search-form');
+mediaForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    await searchDatabase();
+});
